83fc0e8723d0ef59769e5075a021b9a97db98568
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-05-19 19:17:29 +0200

    Add Header component with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import BookList from "./pages/BookList";
@@ -8,6 +9,7 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/books">Book List</Link></li>
+          <li><Link to="/add-book">Add Book</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
