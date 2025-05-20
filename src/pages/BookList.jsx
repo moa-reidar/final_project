762e3a8e4514ff24aa0de7e46083926738055ac7
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-05-20 12:58:17 +0200

    Add access control to BookList page based on login status

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,12 +1,25 @@
 function BookList() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  if (!isLoggedIn) {
     return (
       <div>
-        <h1>Book List</h1>
-        <p>Here you will see a list of all books in the library.</p>
+        <h1>Ingen tilgang</h1>
+        <p>Du må være logget inn for å se boklisten.</p>
       </div>
     );
   }
-  
-  export default BookList;
 
-  
+  return (
+    <div>
+      <h1>Book List</h1>
+      <ul>
+        <li>Harry Potter</li>
+        <li>Lord of the Rings</li>
+        <li>The Hobbit</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
