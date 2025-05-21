2d9b0cd8f96dfd09967ca7384feba44ed86e173d
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-05-22 01:42:52 +0200

    Add navigation links to Home page for Login, AddBook and BookList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Welcome to the Book Library App!</p>
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+  return (
+    <div>
+      <h1>Velkommen til Book Library App 📚</h1>
+      <p>Her kan du logge inn, legge til bøker og se boklisten din.</p>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">🔐 Logg inn</Link>
+          </li>
+          <li>
+            <Link to="/add-book">➕ Legg til bok</Link>
+          </li>
+          <li>
+            <Link to="/books">📖 Se bokliste</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
