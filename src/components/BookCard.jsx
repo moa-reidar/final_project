b4ee36fe1113f1a1f0173475a12220dd7930efcb
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-05-22 15:31:56 +0200

    Include genre in newBook object on submit and display genre in BookCard component

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,13 +1,11 @@
-function BookCard({ title, author, description, onDelete }) {
+function BookCard({ title, author, description, genre, onDelete }) {
     return (
       <li>
         <h3>{title}</h3>
         <p><strong>Av:</strong> {author}</p>
+        {genre && <p><strong>Sjanger:</strong> {genre}</p>}
         {description && <p><strong>Beskrivelse:</strong> {description}</p>}
         <button onClick={onDelete}>🗑️ Slett</button>
       </li>
     );
   }
-  
-  export default BookCard;
-  
