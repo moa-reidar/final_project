fd6b665976d9262c52ee0981139788f7a19e1d9a
Mohammed Reidar Sellami <mohammedreidar@Mohammeds-MacBook-Pro.local>
2025-06-04 19:51:22 +0200

    Set up .env and secured Firestore config

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmdzUFCDS_Q7Tv16jP9TgqRn2xig9sSk",
-  authDomain: "book-library-f6916.firebaseapp.com",
-  projectId: "book-library-f6916",
-  storageBucket: "book-library-f6916.appspot.com",
-  messagingSenderId: "385915205832",
-  appId: "1:385915205832:web:7286d09a43c9965c334dfd",
-  measurementId: "G-LMJRZ4PG9E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
